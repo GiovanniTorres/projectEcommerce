c8f8a57e0924a9480af1c35c3d8e0f36cadfc085
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-22 09:52:14 -0600

    Actualizar index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-// Requiere el archivo de Autoload.php para manejar la carga automática de clases
+
 require_once("./controllers/Autoload.php");
-
-// Instancia de la clase Autoload para registrar el autoloading
 $autoload = new Autoload();4
-
-// Obtiene la ruta de la solicitud, por defecto es 'home'
 $route = isset($_GET['r']) ? $_GET['r'] : 'home';
-
-// Instancia el enrutador con la ruta solicitada
 $router = new Router($route);
