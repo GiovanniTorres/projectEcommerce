79dc9daddd00ee796b0702e9df71f7e4972074d9
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-22 09:58:35 -0600

    Actualizar index.php

--- index.php
+++ index.php
@@ -3,3 +3,13 @@ require_once("./controllers/Autoload.php");
 $autoload = new Autoload();4
 $route = isset($_GET['r']) ? $_GET['r'] : 'home';
 $router = new Router($route);
+
+
+
+
+<?php
+
+require_once("./controllers/Autoload.php");
+$autoload = new Autoload();
+$route = isset($_GET['r']) ? $_GET['r'] : 'home';
+$router = new Router($route);
