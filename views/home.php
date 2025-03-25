ec32e9bbb103002193b04dbea55bb9a542c86bac
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-25 06:50:52 -0600

    Actualizar home.php

--- views/home.php
+++ views/home.php
@@ -1,10 +1,5 @@
-<main class="contenido">
-    <h2>Sección Principal</h2>
-    <p>Bienvenido a mi sitio web. Aquí encontrarás información interesante.</p>
-
-    <div class="contenedor">
-        <div class="caja">Caja 1</div>
-        <div class="caja">Caja 2</div>
-        <div class="caja">Caja 3</div>
-    </div>
+<main class="contenedor">
+    <div class="caja">Div 1</div>
+    <div class="caja">Div 2</div>
+    <div class="caja">Div 3</div>
 </main>
