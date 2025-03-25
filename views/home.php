668f06ca4d1f9022d10bc31a6a8d58d9b08f4b99
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-25 08:21:52 -0600

    Actualizar home.php

--- views/home.php
+++ views/home.php
@@ -1,5 +1,7 @@
-<main class="contenedor">
-    <div class="caja">Div 1</div>
-    <div class="caja">Div 2</div>
-    <div class="caja">Div 3</div>
+<main>
+    <div class="container">
+        <div class="box">Contenido 1</div>
+        <div class="box">Contenido 2</div>
+        <div class="box">Contenido 3</div>
+    </div>
 </main>
