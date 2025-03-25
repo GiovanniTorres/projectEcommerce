21cd20cacfa21b374039625a05d199e75e7047eb
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-25 12:46:54 -0600

    Actualizar home.php

--- views/home.php
+++ views/home.php
@@ -2,6 +2,6 @@
     <div class="container">
         <div class="box">Contenido 1</div>
         <div class="box">Contenido 2</div>
-        <div class="box">Contenido 3</div>
+        <!--div class="box">Contenido 3</div-->
     </div>
 </main>
