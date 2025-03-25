f595700bde7207ca012731d32b17f0111beb4065
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-25 10:47:16 -0600

    Actualizar script.js

--- public/script.js
+++ public/script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+// Cierra el menú si haces clic fuera de él
+document.addEventListener("click", function (event) {
+    const menu = document.querySelector(".menu");
+    const menuToggle = document.querySelector(".menu-toggle");
+
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+        menu.classList.remove("active"); // Se cierra el menú
+    }
+});
