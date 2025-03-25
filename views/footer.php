03b308edf1a4d24ce4fd80b0f21d3fda5a3c89c7
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-25 08:22:21 -0600

    Actualizar footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,7 @@
-<footer class="footer">
-    <p>© 2025 - Mi Sitio Web</p>
+<footer>
+    <p>&copy; 2025 Mi Sitio Web</p>
 </footer>
 
-
-<script src="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>/script.js"></script>
- <!-- Ruta corregida -->
-
+<script src="../public/script.js"></script>
 </body>
 </html>
